7517e8c931bb51a113fefa7c2ceed16c7c3b7dca
xbbbbbbimbiker <krnlrobloxacc228@gmail.com>
2026-04-22 11:28:17 +0300

    добавил код в car.cpp

--- car_project/car.cpp
+++ car_project/car.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+struct Car {
+    string brand;
+    string model;
+    int year;
+    int horsepower;
+
+    
+    void printInfo() const {
+        cout << "Автомобиль: " << brand << " " << model << endl;
+        cout << "Год выпуска: " << year << endl;
+        cout << "Мощность: " << horsepower << " л.с." << endl;
+    }
+};
+
+double powerToKW(int hp) {
+    return hp * 0.735;
+}
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    Car myCar;
+    myCar.brand = "Lada";
+    myCar.model = "Vesta";
+    myCar.year = 2023;
+    myCar.horsepower = 106;
+
+    myCar.printInfo();
+
+    cout << "Мощность в киловаттах: " << powerToKW(myCar.horsepower) << " кВт" << endl;
+}
